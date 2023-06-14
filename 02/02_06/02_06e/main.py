958f8f9936eb39544b07c65c224d7f9e84efa2bf
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 17:52:33 +0000

    tuples

--- 02/02_06/02_06e/main.py
+++ 02/02_06/02_06e/main.py
@@ -1,1 +1,15 @@
 # Tuples are immutable array-like structures
+
+point = (5, 2)
+
+x = point[0]
+y = point[1]
+
+def calculate_square_properties(side_length):
+    area = side_length * side_length
+    perimeter = 4 * side_length
+    return (area, perimeter)
+
+result = calculate_square_properties(5)
+print("Area: ", result[0])
+print("Perimeter ", result[1])
