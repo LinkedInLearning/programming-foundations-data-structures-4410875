1bf03e1cb3e82193c9fac1480d126564dedf7709
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 18:17:38 +0000

    02_08

--- 02/02_08/02_08e/main.py
+++ 02/02_08/02_08e/main.py
@@ -1,2 +1,6 @@
 my_list = [1, 7, 3]
-print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+
+student_grades = [('Sarah', 89), ('Rebecca', 82), ('Matt', 91)]
+print(sorted(student_grades))
+print(sorted(student_grades, key=lambda x:x[1], reverse=True))
