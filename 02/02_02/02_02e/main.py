7d173840e1d3a3a97a70dcf470a44c69350cfabc
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 17:04:45 +0000

    implement

--- 02/02_02/02_02e/main.py
+++ 02/02_02/02_02e/main.py
@@ -2,3 +2,10 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+
+student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
+
+NUM_OF_STUDENTS = len(student_pet_count_list)
+print(NUM_OF_STUDENTS)
+
+# average = sum / number of items
--- 02/02_03/02_03e/main.py
+++ 02/02_03/02_03e/main.py
@@ -3,20 +3,9 @@ Problem Statement: Compute the average number of pets each student
 has in a given class. 
 '''
 
-student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
+student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
-# Indexing
-item_at_index_three = student_pet_count_list[3]
-# print(student_pet_count_list[100])
-item_three_from_back = student_pet_count_list[-3]
+NUM_OF_STUDENTS = len(student_pet_count_list)
+print(NUM_OF_STUDENTS)
 
-sum = 0
-for individual_pet_count in student_pet_count_list:
-    sum = sum + individual_pet_count
-
-num_of_students = len(student_pet_count_list)
-print(num_of_students)
-
-# sum_of_items / number_of_items
-average = sum / num_of_students
-print(average)
+# average = sum / number of items
