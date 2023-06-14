d471eb090367c8570b3790753dba7d69ea340fa2
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 19:19:34 +0000

    03_03

--- 03/03_02/03_02e/main.py
+++ 03/03_02/03_02e/main.py
@@ -1,2 +1,13 @@
 # Key: State
 # Value: Capital
+
+states_to_capitals = {
+    "Texas" : "Austin",
+    "New York" : "Albany"
+}
+
+print(states_to_capitals["New York"])
+
+for key, value in states_to_capitals.items():
+    print(key + " | " + value)
+    
---
+++ 03/03_03/03_03e/main.py
@@ -0,0 +1,9 @@
+user_preferences = {
+    "language": "English",
+    "font_size": "14px",
+    "timezone": "GMT",
+    "curerncy": "USD",
+    "enable_location": False,
+    "volume_level": 80,
+    "date_format": "MM/DD/YYYY"
+}
