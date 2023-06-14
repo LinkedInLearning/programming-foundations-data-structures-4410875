a46c220115f2f7c314003b14d683dce2d7bc44eb
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 17:13:28 +0000

    add

--- 02/02_04/02_04b/main.py
+++ 02/02_04/02_04b/main.py
@@ -5,10 +5,6 @@ has in a given class.
 
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
-ITEM_AT_INDEX_THREE = student_pet_count_list[3]
-# print(student_pet_count_list[100])
-ITEM_THREE_FROM_BACK = student_pet_count_list[-3]
-
 NUM_OF_STUDENTS = len(student_pet_count_list)
 print(NUM_OF_STUDENTS)
 SUM = 0
