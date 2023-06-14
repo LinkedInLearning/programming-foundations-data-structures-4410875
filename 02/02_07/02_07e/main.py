43b78555846c67d84bc848e51a651fa59f1c63f7
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 18:07:16 +0000

    02_08

--- 02/02_07/02_07e/main.py
+++ 02/02_07/02_07e/main.py
@@ -1,1 +1,14 @@
 # Linear Search
+
+my_list = [8, 5, 0, 3, 9, 7]
+ITEM = 7
+
+def search(item, listy):
+    for element in listy:
+        if element == item:
+            return True
+    return False
+
+print(search(ITEM, my_list))
+
+ITEM_INDEX = my_list.index(ITEM)
---
+++ 02/02_08/02_08e/main.py
@@ -0,0 +1,2 @@
+my_list = [1, 7, 3]
+print(sorted(my_list))
