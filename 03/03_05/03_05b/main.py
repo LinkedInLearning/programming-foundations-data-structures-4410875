e03daa2c73a653bd484a1ee52937e09563d181ab
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 20:56:33 +0000

    fix

---
+++ 03/03_05/03_05b/main.py
@@ -0,0 +1,14 @@
+user_preferences = {
+    "timezone": "GMT",
+    "language": "English",
+    "notifications": None,
+    "currency": "USD",
+    "theme": None
+}
+
+
+def update_preferences(user_pref):
+    return {}
+
+
+print(update_preferences(user_preferences))
