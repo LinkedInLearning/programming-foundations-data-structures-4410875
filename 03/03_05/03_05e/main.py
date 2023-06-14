03d1a6521a940eca576b3d27bcc348f31a1114be
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 21:05:37 +0000

    add

--- 03/03_05/03_05e/main.py
+++ 03/03_05/03_05e/main.py
@@ -7,7 +7,11 @@ user_preferences = {
 }
 
 def update_preferences(user_pref):
-    return {}
-
+    return {key: value for key, value in user_pref.items() if value is not None }
+    # updated_preferences = {}
+    # for key, value in user_pref.items():
+    #     if value is not None:
+    #         updated_preferences[key] = value
+    # return updated_preferences
 
 print(update_preferences(user_preferences))
