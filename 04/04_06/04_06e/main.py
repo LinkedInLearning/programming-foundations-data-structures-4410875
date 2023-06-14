7d11e93480105cae3a9ed07838ac1aaa8fa05ee5
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 23:18:17 +0000

    add

---
+++ 04/04_06/04_06e/main.py
@@ -0,0 +1,7 @@
+def has_unique_characters(data):
+    return False
+
+print(has_unique_characters('sample'))
+print(has_unique_characters('hello world'))
+print(has_unique_characters('linkedin'))
+print(has_unique_characters('python'))
