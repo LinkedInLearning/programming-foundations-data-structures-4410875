81328f16e1a419382b77ead170afe8ff16e5a03e
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 18:41:22 +0000

    challenge

--- 02/02_10/02_10e/main.py
+++ 02/02_10/02_10e/main.py
@@ -1,4 +1,20 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+    smallest = float('inf')
+    second_smallest = float('inf')
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+    return second_smallest
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
