b7b24bf39bb2a83f0ba1ffcf0c2d762e09afceea
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 20:29:12 +0000

    setup

--- 03/03_03/03_03e/main.py
+++ 03/03_03/03_03e/main.py
@@ -2,7 +2,7 @@ user_preferences = {
     "language": "English",
     "font_size": "14px",
     "timezone": "GMT",
-    "curerncy": "USD",
+    "currency": "USD",
     "enable_location": False,
     "volume_level": 80,
     "date_format": "MM/DD/YYYY"
