0beabe4c9b73a4fccb00c297abc4d335b7adbed9
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 17:30:28 +0000

    Implement

--- 02/02_04/02_04e/main.py
+++ 02/02_04/02_04e/main.py
@@ -5,6 +5,12 @@ has in a given class.
 
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
+student_pet_count_list[2] = 3
+student_pet_count_list[3] = student_pet_count_list[3] + 1
+student_pet_count_list[-1] = student_pet_count_list[-1] + 2
+
+student_pet_count_list.append(4)
+
 NUM_OF_STUDENTS = len(student_pet_count_list)
 print(NUM_OF_STUDENTS)
 SUM = 0
---
+++ 02/02_05/02_05b/main.py
@@ -0,0 +1,6 @@
+seating_chart = [
+    ["Sarah", "CLaire", "Ben", "Taylor", "Eva"],
+    ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
+    ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
+    ["Chad", "April", "Matt", "Thomas", "Penny"]
+]
