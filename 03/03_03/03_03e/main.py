34525049a0479d625596c30b55efc7bf2e33812c
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 20:38:28 +0000

    add changes

--- 03/03_03/03_03e/main.py
+++ 03/03_03/03_03e/main.py
@@ -7,3 +7,13 @@ user_preferences = {
     "volume_level": 80,
     "date_format": "MM/DD/YYYY"
 }
+
+user_preferences["language"] = "Spanish"
+
+user_preferences["volume_level"] = 50
+user_preferences["highlight_color"] = "yellow"
+
+del user_preferences["currency"]
+removed_item = user_preferences.pop("date_format", "N/A")
+
+print(user_preferences)
