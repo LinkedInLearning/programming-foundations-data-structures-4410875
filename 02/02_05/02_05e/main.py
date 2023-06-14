c0ea75491f76cfdfde56bac23856f166e931a359
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-14 17:44:58 +0000

    add 02_06

--- 02/02_05/02_05e/main.py
+++ 02/02_05/02_05e/main.py
@@ -4,3 +4,9 @@ seating_chart = [
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
     ["Chad", "April", "Matt", "Thomas", "Penny"]
 ]
+
+# print(seating_chart[2][1])
+
+for i, row in enumerate(seating_chart):
+    for j, student_name in enumerate(row):
+        print(f"{student_name} is in row {i+1}, seat {j+1}")
---
+++ 02/02_06/02_06e/main.py
@@ -0,0 +1,1 @@
+# Tuples are immutable array-like structures
