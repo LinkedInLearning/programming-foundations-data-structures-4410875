3be5db93ca1f6bb7c18941803d02d5df9557ca56
Kathryn Hodge <blondiebytes@gmail.com>
2023-06-15 17:27:55 +0000

    add

--- 06/06_03/06_03e/main.py
+++ 06/06_03/06_03e/main.py
@@ -1,1 +1,9 @@
 from collections import deque
+
+history_stack = deque()
+history_stack.append("https://google.com")
+history_stack.append("https://linkedin.com")
+history_stack.append("https://stackoverflow.com")
+
+print(history_stack[-1])
+print(history_stack.pop())
